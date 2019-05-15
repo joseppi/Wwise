b9df6a5145fc489be703b47ca1027be944ac724f
joseppi <josep.pi.serra@gmail.com>
2019-05-15 14:47:37 +0200

    Added env sounds

---
+++ WwiseProject3/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,109 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ALITA_AIR_EFFORTS_KICK_STRONG = 3105098143U;
+        static const AkUniqueID PLAY_ALITA_AIR_EFFORTS_KICK_WEAK = 77725278U;
+        static const AkUniqueID PLAY_ALITA_AIR_EFFORTS_PUNCH_STRONG = 3837585939U;
+        static const AkUniqueID PLAY_ALITA_AIR_EFFORTS_PUNCH_WEAK = 1458427522U;
+        static const AkUniqueID PLAY_ALITA_AIR_EFFORTS_SKILL_PUNCH = 3954361721U;
+        static const AkUniqueID PLAY_ALITA_DASH = 2834343218U;
+        static const AkUniqueID PLAY_ALITA_DEATH_GRUNT = 42272559U;
+        static const AkUniqueID PLAY_ALITA_DIALOGUE = 2043804372U;
+        static const AkUniqueID PLAY_ALITA_DODGE = 186020995U;
+        static const AkUniqueID PLAY_ALITA_FOOTSTEPS = 2920149871U;
+        static const AkUniqueID PLAY_ALITA_HIT = 2322423541U;
+        static const AkUniqueID PLAY_ALITA_HURT = 3841648673U;
+        static const AkUniqueID PLAY_ALITA_KICK_AIR_STRONG = 855771973U;
+        static const AkUniqueID PLAY_ALITA_KICK_AIR_WEAK = 2144988368U;
+        static const AkUniqueID PLAY_ALITA_PUNCH_AIR_STRONG = 3409856023U;
+        static const AkUniqueID PLAY_ALITA_PUNCH_AIR_WEAK = 1780127622U;
+        static const AkUniqueID PLAY_ALITA_SKILL_AXE = 3759221212U;
+        static const AkUniqueID PLAY_ALITA_SKILL_CHARGE = 4128801052U;
+        static const AkUniqueID PLAY_ALITA_SKILL_GUN = 3877650214U;
+        static const AkUniqueID PLAY_ALITA_SKILL_PUNCH = 2580785920U;
+        static const AkUniqueID PLAY_BGM_PLACEHOLDER = 1974026970U;
+        static const AkUniqueID PLAY_COMBAT_MUSIC = 3155474038U;
+        static const AkUniqueID PLAY_ENEMY_DIALOGUE = 3053813775U;
+        static const AkUniqueID PLAY_GUI_DIALOG = 1904526526U;
+        static const AkUniqueID PLAY_GUI_HOVER = 716998164U;
+        static const AkUniqueID PLAY_GUI_OPEN_MENU = 2572159320U;
+        static const AkUniqueID PLAY_GUI_SKILLPOINT = 2557756721U;
+        static const AkUniqueID PLAY_GUI_UNLOCKED = 1337817101U;
+        static const AkUniqueID PLAY_MARAUDER_DEATH = 1801786362U;
+        static const AkUniqueID PLAY_MARAUDER_DETECTED = 2620597494U;
+        static const AkUniqueID PLAY_MARAUDER_FOOTSTEPS = 2999633473U;
+        static const AkUniqueID PLAY_MARAUDER_GUNSHOT = 106314442U;
+        static const AkUniqueID PLAY_MARAUDER_RECEIVE_DAMAGE_WEAK = 3245151616U;
+        static const AkUniqueID PLAY_MARAUDER_RECIEVE_DAMAGE_STRONG_KICK = 3879597586U;
+        static const AkUniqueID PLAY_MARAUDER_RECIEVE_DAMAGE_STRONG_PUNCH = 220735744U;
+        static const AkUniqueID PLAY_MINE_EXPLOSION = 2109787835U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GROUND
+        {
+            static const AkUniqueID GROUP = 2528658256U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ASPHALT = 4169408098U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace GROUND
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PITCH = 1908158473U;
+        static const AkUniqueID VOLUME = 2415836739U;
+        static const AkUniqueID VOLUME_BGM = 3659619896U;
+        static const AkUniqueID VOLUME_MASTER = 3695994288U;
+        static const AkUniqueID VOLUME_SFX = 3673881719U;
+        static const AkUniqueID VOLUME_VO = 4161707587U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BGM = 412724365U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VO = 1534528548U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID GUI = 915214414U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VO = 1534528548U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
